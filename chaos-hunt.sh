b9ac57b8c70faa7f29eafccb5c52dea2fa9cd672
Rohit Gautam <18241082+shifa123@users.noreply.github.com>
2020-09-26 12:04:33 +0530

    Update chaos-hunt.sh

--- chaos-hunt.sh
+++ chaos-hunt.sh
@@ -1,1 +1,1 @@
-curl "https://chaos-data.projectdiscovery.io/index.json" && cat index.json | grep "URL" | sed 's/"URL": "//;s/",//' | while read host do;do wget "$host";done && for i in `ls -1 | grep .zip$`; do unzip $i; done && rm *.zip && cat *.txt >> alltargets.txt
+wget "https://chaos-data.projectdiscovery.io/index.json" && cat index.json | grep "URL" | sed 's/"URL": "//;s/",//' | while read host do;do wget "$host";done && for i in `ls -1 | grep .zip$`; do unzip $i; done && rm *.zip && cat *.txt >> alltargets.txt
